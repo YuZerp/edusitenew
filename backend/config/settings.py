42255ce3f29d2384d4fc2166e25c0ae0ebc817e2
YuZerp <yuzerp@gmail.com>
2026-05-31 21:46:06 +0500

    new settings

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -136,7 +136,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://edusitenew-oc6yc0ret-yuzerps-projects.vercel.app",
+    "https://edusitenew.vercel.app",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
