ce138cbb590370ff8bb311cfc2faa636d1e40064
YuZerp <yuzerp@gmail.com>
2026-05-31 22:18:22 +0500

    fix: добавлены URL маршруты для API

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,5 +3,4 @@ from .views import ApplicationListCreateView, ApplicationDetailView
 
 urlpatterns = [
     path('applications/', ApplicationListCreateView.as_view(), name='application-list'),
-    path('applications/<int:pk>/', ApplicationDetailView.as_view(), name='application-detail'),
 ]
