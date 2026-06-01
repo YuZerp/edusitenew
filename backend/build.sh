10e65f0eb2a05b6a0609e300ccb3bd5046591cad
YuZerp <yuzerp@gmail.com>
2026-06-01 05:33:36 +0500

    new superuser

--- backend/build.sh
+++ backend/build.sh
@@ -2,3 +2,8 @@
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
+
+if [[ $CREATE_SUPERUSER ]];
+then
+    python manage.py createsuperuser --no-input --username admin --email admin@example.com
+fi
